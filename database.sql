f4d4d72dd99608e77456067bc660216866fd890b
Eragon <eragon@eragon.re>
2021-10-23 19:02:16 +0200

    Proper database init script

--- database.sql
+++ database.sql
@@ -1,83 +1,100 @@
-CREATE TABLE user (
-    id int ++,
-    username string,
-    name string,
-    firstname string,
-    password string
-)
+-- Drop conflicting tables
+DROP TABLE IF EXISTS "article_join_commande";
+DROP TABLE IF EXISTS "article_join_fournisseur";
+DROP TABLE IF EXISTS "hist_join_com";
+DROP TABLE IF EXISTS "client";
+DROP TABLE IF EXISTS "salarie";
+DROP TABLE IF EXISTS "fournisseur";
+DROP TABLE IF EXISTS "commande";
+DROP TABLE IF EXISTS "user";
+DROP TABLE IF EXISTS "article";
+DROP TABLE IF EXISTS "adresse";
 
+-- Adresses
+CREATE TABLE "adresse" (
+    "id" SERIAL PRIMARY KEY,
+    "codepostal" VARCHAR(10) NOT NULL,
+    "nomrue" VARCHAR(255) NOT NULL,
+    "voie" VARCHAR(255),
+    "pays" VARCHAR(255) NOT NULL,
+    "ville" VARCHAR(255) NOT NULL
+);
 
-CREATE TABLE salarie
-(
-    userId: int ++ /* meme id que user */
-    poste string
+-- Articles/Products
+CREATE TABLE "article" (
+    "id" SERIAL PRIMARY KEY,
+    "nom" VARCHAR(255) NOT NULL,
+    "stock" INT NOT NULL,
+    "description" text,
+    "type" VARCHAR(255) NOT NULL,
+    "provenance" VARCHAR(255) NOT NULL,
+    "medaille" VARCHAR(255),
+    "annee" DATE NOT NULL,
+    "domaine" VARCHAR(255) NOT NULL
+);
 
-)
+-- User
+CREATE TABLE "user" (
+    "id" SERIAL PRIMARY KEY,
+    "username" VARCHAR(255) NOT NULL UNIQUE,
+    "lastname" VARCHAR(255) NOT NULL,
+    "firstname" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL
+);
 
-CREATE TABLE histcom /*table de jointure historique commande*/
-(
-    userID  int
-    commmandeid int ++
-)
+-- Commands
+CREATE TABLE "commande" (
+    "id" SERIAL PRIMARY KEY,
+    "datecommande" DATE NOT NULL,
+    "datelivraison" DATE,
+    "quantite" INT NOT NULL,
+    "prix" NUMERIC NOT NULL
+);
 
-CREATE TABLE client
-(
-    userID int
-    adressID int
-    email string
-)
+-- Fournisseurs
+CREATE TABLE "fournisseur" (
+    "id" SERIAL PRIMARY KEY,
+    "nom" VARCHAR(255) NOT NULL,
+    "adresse" INT NOT NULL UNIQUE,
+    "telephone" VARCHAR(20) UNIQUE
+);
 
-CREATE TABLE adresse
-(
-  id int ++
-  codePostal string
-  nomRue string,
-  numRue string,
-  voie  string,
-  pays: string
-  ville string
-)
+-- Salaries
+CREATE TABLE "salarie" (
+    "user" INT NOT NULL UNIQUE,
+    "poste" VARCHAR(255) NOT NULL,
+	FOREIGN KEY ("user") REFERENCES "user" ("id")
+);
 
-CREATE TABLE article
-(
- nomProduit string
- fournisseur string,
- id int ++,
- stock int,
- description string,
- typeVin string,
- provenance string,
- medaille string,
- annee int,
- domaine string,
-CREATE TABLE fournisseur
-(
- id int ++
- nom string,
- adresseId int,
- telephone string
+-- Clients
+CREATE TABLE "client" (
+    "user" INT NOT NULL UNIQUE,
+    "adresse" INT NOT NULL,
+    "email" VARCHAR(255) NOT NULL UNIQUE,
+	FOREIGN KEY ("user") REFERENCES "user" ("id"),
+	FOREIGN KEY ("adresse") REFERENCES "adresse" ("id")
+);
 
-)
+-- With that we can know the articles in a command
+CREATE TABLE "article_join_commande" (
+    "article" INT NOT NULL,
+    "commande" INT NOT NULL,
+	FOREIGN KEY ("article") REFERENCES "article" ("id"),
+	FOREIGN KEY ("commande") REFERENCES "commande" ("id")
+);
 
-CREATE TABLE commande
-(
-    id int ++
-    articleId,
-    dateCommande int (date),
-    quantite int,
-    dateLivraison int (date),
-    prix float
-)
+-- With this one we know whitch fournisseur have witch article
+CREATE TABLE "article_join_fournisseur" (
+    "article" INT NOT NULL,
+    "fournisseur" INT NOT NULL,
+	FOREIGN KEY ("article") REFERENCES "article" ("id"),
+	FOREIGN KEY ("fournisseur") REFERENCES "fournisseur" ("id")
+);
 
-CREATE TABLE articleFournisseur /*table de jointure */
-(
-articleId int 
-fournisseurId int
-)
-
-CREATE TABLE articleCommande
-(
-articleId string,
-commandeId string
-)
-
+-- This is the historic of commands from the users
+CREATE TABLE "hist_join_com" (
+    "user" INT NOT NULL,
+    "commande" INT NOT NULL,
+	FOREIGN KEY ("user") REFERENCES "user" ("id"),
+	FOREIGN KEY ("commande") REFERENCES "commande" ("id")
+);
