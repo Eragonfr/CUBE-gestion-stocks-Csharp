900aa110517f33a3a4626ec5fac5f1772d511621
Eragon <eragon@eragon.re>
2021-11-09 11:54:23 +0100

    Connection to database

---
+++ server/Models/User.cs
@@ -0,0 +1,11 @@
+namespace Users.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+		public string Lastname { get; set; }
+		public string Firstname { get; set; }
+		public string Password { get; set; }
+    }
+}
